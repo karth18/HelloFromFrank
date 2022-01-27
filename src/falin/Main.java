b34cae6c53e84238a5a42139604c7b1eff702a7c
karth18 <frank.arth.5@gmail.com>
2022-01-27 07:00:16 -0500

    Initial commit

---
+++ src/falin/Main.java
@@ -0,0 +1,12 @@
+package falin;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String name = "Frank";
+
+        System.out.println("Hello, my name is " + name);
+
+
+    }
+}
